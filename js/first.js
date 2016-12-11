a4c624d21007c21173a99da18714618842d41a27
u4bi <u4bi@u4bi.com>
2016-12-11 06:43:56 +0000

    first.js - body svg circle select

--- js/first.js
+++ js/first.js
@@ -1,1 +1,24 @@
 /* first.js */
+
+d3.select("body")
+  .append("svg")
+  .attr("width", 50)
+  .attr("height", 50)
+  .append("circle")
+  .attr("cx", 25)
+  .attr("cy", 25)
+  .attr("r", 25)
+  /* style operator*/
+  .style("fill", "red");
+
+var bodySelect = d3.select("body");
+
+var svgSelect = bodySelect.append("svg")
+  .attr("width", 50)
+  .attr("height", 50);
+
+var circleSelect = svgSelect.append("circle")
+  .attr("cx", 25)
+  .attr("cy", 25)
+  .attr("r", 25)
+  .style("fill", "green");
