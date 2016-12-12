6c04f0e2e729d1e105571ffad0d70faaeab001f7
u4bi <u4bi@u4bi.com>
2016-12-12 13:19:31 +0000

    bar chart - hover init , fix fix scaleY

--- chart/js/bar-chart.js
+++ chart/js/bar-chart.js
@@ -1,14 +1,14 @@
 /* bar-chart.js*/
 
 var data = [
-  {'name': '가', 'value' : .8167},
-  {'name': '나', 'value' : .4167},
-  {'name': '다', 'value' : .3167},
-  {'name': '라', 'value' : .6167},
-  {'name': '마', 'value' : .3167},
-  {'name': '바', 'value' : .5167},
-  {'name': '사', 'value' : .3167},
-  {'name': '아', 'value' : .1167}
+  {'name': '홍길', 'value' : 67},
+  {'name': '홍순', 'value' : 53},
+  {'name': '남순', 'value' : 84},
+  {'name': '남돌', 'value' : 33},
+  {'name': '돌훈', 'value' : 21},
+  {'name': '도한', 'value' : 15},
+  {'name': '도순', 'value' : 89},
+  {'name': '한길', 'value' : 71}
 ];
 
 var info = {
@@ -32,7 +32,7 @@ var x = d3.scale.ordinal()
 
 /* y의 scale 정의*/
 var y = d3.scale.linear()
-  .domain([0, d3.max(data, function(d){return d.value; })]) /* d3,max() 어레이내 최대값*/
+  .domain([0, 100]) /* 0부터 100까지*/
   .range([info.height, 0]); /* 높이지정*/
 
 /* x의 axis 정의*/
@@ -43,20 +43,21 @@ var xAxis = d3.svg.axis() /* axis 호출 함수 선언*/
 /* y의 axis 정의*/
 var yAxis = d3.svg.axis() /* axis 호출 함수 선언*/
   .scale(y) /* y내 정의된 값으로 초기화*/
-  .orient('left') /* 레프트에 위치*/
-  .ticks(10, "%"); /* 정의된 문자 변환*/
+  .orient('left'); /* 레프트에 위치*/
+  // .ticks(10, "%"); /* 정의된 문자 변환*/
 
 /* 클래스 .chart가 선언된 태그의 너비와 높이를 정의함*/
 var chart = d3.select('.chart') /* .char 클래스가 선언된 태그를 찾음*/
   .attr('width', info.width + info.margin.left + info.margin.right) /* width값 정의해줌 일전에 빼둔 margin값 더하여 실제 너비값으로 초기화*/
   .attr('height', info.height + info.margin.top + info.margin.bottom) /* height값 정의해줌 일전에 빼둔 margin값 더하여 실제 높이값으로 초기화*/
+  .style('border', '1px solid black') /* css 속성 보더주입*/
   .append('g') /* g 그룹태그를 어펜드해줌*/
   .attr('transform', 'translate(' + info.margin.left + ',' + (info.margin.top) + ')'); /* 선언된 컨테이너의 마진 조정함*/
 
 chart.append('g') /* 차트 컨테이너에 g 그룹태그를 어팬드함*/
-    .attr('class', 'x axis') /* axis라는 클래스 주입 style.css에서 가져옴*/
-    .attr('transform', 'translate(0,' + info.height + ')') /*translate로 부착될 위치조정 화면아래 즉 차트아래*/
-    .call(xAxis); /* 정의된 xAxis 호출하여 초기화함*/
+  .attr('class', 'x axis') /* axis라는 클래스 주입 style.css에서 가져옴*/
+  .attr('transform', 'translate(0,' + info.height + ')') /*translate로 부착될 위치조정 화면아래 즉 차트아래*/
+  .call(xAxis); /* 정의된 xAxis 호출하여 초기화함*/
 
 chart.append('g') /* 차트 컨테이너에 g 그룹태그를 어팬드함*/
     .attr('class', 'y axis') /* axis라는 클래스 주입 style.css에서 가져옴*/
